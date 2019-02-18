cf082c8a759fe1b3fcbe116cb47d1a1ad7e546fc
Sócrates Díaz <sdsxd1800@gmail.com>
2019-02-18 15:07:06 -0400

    Optimized sequence full of colors: It works now with only one loop over each string. Created a custom absolute value function to avoid reflection drawbacks of Math/abs.

--- src/hackerrank_exes/sequence_full_of_colors.clj
+++ src/hackerrank_exes/sequence_full_of_colors.clj
@@ -1,28 +1,27 @@
 (ns hackerrank-exes.sequence-full-of-colors)
 
-(defn read-input [] (line-seq (java.io.BufferedReader. *in*)))
+;; (defn read-input [] (line-seq (java.io.BufferedReader. *in*)))
+(defn read-input [] (line-seq (java.io.BufferedReader. (java.io.FileReader. "input06.txt"))))
 
-(defn prefixers [coll]
-  (map #(take % coll) (range 1 (count coll))))
+(defn abs [x] (if (< x 0) (* x -1) x))
 
-(defn process-input [input]
-  (let [fn-converter (fn [c] (case c \R :R \G :G \Y :Y \B :B))
-        values       (rest input)
-        values       (map #(map fn-converter %) values)]
-    values))
+(defn solve [full-string]
+  (let [fn-diff (fn [balls-count] (and (<= (abs (- (balls-count :R 0) (balls-count :G 0))) 1)
+                                      (<= (abs (- (balls-count :Y 0) (balls-count :B 0))) 1)))]
+    (loop [string full-string
+           c      (first string)
+           balls  {:R 0 :G 0 :Y 0 :B 0}]
+      (if (nil? c)
+        (and
+         (= (balls :R 0) (balls :G 0))
+         (= (balls :Y 0) (balls :B 0)))
+        (if-not (fn-diff balls)
+          false
+          (let [balls (assoc balls
+                             (keyword (str c))
+                             (inc ((keyword (str c)) balls)))]
+            (recur (rest string) (second string) balls)))))))
 
-(defn solve [input]
-  (let [values  (frequencies input)
-        fn-diff (fn [n] (and (<= (Math/abs (- (get n :R 0) (get n :G 0))) 1)
-                             (<= (Math/abs (- (get n :Y 0) (get n :B 0))) 1)))]
-    (and
-     (= (get values :R 0) (get values :G 0))
-     (= (get values :Y 0) (get values :B 0))
-     (let [prefixes (prefixers input)
-           prefixes (map frequencies prefixes)]
-       (apply (every-pred fn-diff) prefixes)))))
-
-(time (let [input  (read-input)
-            values (process-input input)
-            solved (map solve values)]
+(time (let [input  (rest (read-input))
+            solved (map solve input)]
         (doall (map #(println (if % "True" "False")) solved))))
