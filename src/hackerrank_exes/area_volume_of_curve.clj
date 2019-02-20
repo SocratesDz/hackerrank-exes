19dbb4d2dd29cd138c4411731c29ac9d483b6192
Sócrates Díaz <sdsxd1800@gmail.com>
2019-02-20 00:56:19 -0400

    Added fixes and optimizations

--- src/hackerrank_exes/area_volume_of_curve.clj
+++ src/hackerrank_exes/area_volume_of_curve.clj
@@ -5,10 +5,6 @@
 
 (defn exp [n m] (Math/pow ^double n ^double m))
 
-(defn dx [a b n] (/ (- b a) n))
-
-(defn c [a b i n] (+ a (* (dx a b n) i)))
-
 (defn formula [factor exponent]
   (fn [x] (* factor (exp x exponent))))
 
@@ -17,23 +13,19 @@
     (fn [x] (apply + (map #(% x) formulae)))))
 
 (defn solve [factors exponents limits subinterval]
-  (let [a         (first limits)
-        b         (second limits)
-        f         (full-formula factors exponents)
-        c         (partial c a b)
-        dx        (partial dx a b)
-        ns        (range 1 (inc (/ (- b a) subinterval)))
-        area-fn   (fn [i] (* (f (c i (count ns))) (dx (count ns))))
-        volume-fn (fn [i] (* Math/PI (exp (f (c i (count ns))) 2) (dx (count ns))))]
-    [(apply + (map area-fn ns)) (apply + (map volume-fn ns))]))
+  (let [form   (full-formula factors exponents)
+        a      (first limits)
+        b      (second limits)
+        n      (range a (+ b subinterval) subinterval)
+        area   (* subinterval (apply + (map form n)))
+        volume (* subinterval (apply + (map #(* Math/PI (exp (form %) 2)) n)))]
+    [area volume]))
 
 (defn read-input []
   (let [to-int (fn [string] (map #(Integer/parseInt %) (split string #" ")))
         lines  (line-seq (java.io.BufferedReader. *in*))]
     (vec (map to-int lines))))
 
-(def test-data [[1 2] [0 1] [2 20]])
-
-(let [input  (conj test-data subinterval);; (conj (read-input) subinterval)
-      solved (apply solve input)]
+(let [input  (conj (read-input) subinterval)
+      solved (apply solve2 input)]
   (doseq [n solved] (println n)))
